ca9a7cfdd3bf94409573d2dbfbef77bd0eff6622
Paweł Prorok <pawel3474@tlen.pl>
2022-02-06 19:28:21 +0100

    Add icons to tasks list

--- script.js
+++ script.js
@@ -22,11 +22,35 @@ const addNewTodo = () => {
         newTodo = document.createElement('li')
         newTodo.textContent = todoInput.value
         ulList.append(newTodo)
+        createToolsArea()
+
+        ulList.append(newTodo)
+
         todoInput.value = ''
         errorInfo.textContent = ''
     } else {
         errorInfo.textContent = 'Wpisz treść zadania'
     }
+}
+
+const createToolsArea = () => {
+    const toolsPanel = document.createElement('div')
+    toolsPanel.classList.add('tools')
+    newTodo.append(toolsPanel)
+
+    const completeBnt = document.createElement('button')
+    completeBnt.classList.add('complete')
+    completeBnt.innerHTML = '<i class="fas fa-check"></i>'
+
+    const editBnt = document.createElement('button')
+    editBnt.classList.add('edit')
+    editBnt.textContent = 'EDIT'
+
+    const deleteBnt = document.createElement('button')
+    deleteBnt.classList.add('delete')
+    deleteBnt.innerHTML = '<i class="fas fa-times"></i>'
+
+    toolsPanel.append(completeBnt, editBnt, deleteBnt)
 }
 
 document.addEventListener('DOMContentLoaded', main)
